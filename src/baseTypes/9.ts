f9bc52787f7970284ccbe2ca292eecb9f459fa9c
VitaPerevoznikova <frayvita@gmail.com>
2023-12-07 21:05:37 +0200

    sum

--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -20,4 +20,15 @@ const page2 = {
   status: 'close',
 }
 
+type Page ={
+  title: string;
+  likes: number;
+  accounts: string[];
+  status: "open" | "close";
+  details?: {
+    createAt: Date;
+    updateAt: Date;
+  };
+}
+
 export {};
